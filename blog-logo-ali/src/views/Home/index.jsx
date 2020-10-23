8e15d103bb05ca76ae3409b2458460214475d9d2
goncalves-ac <goncalves.ac@outlook.com>
2020-10-23 11:38:49 -0300

    css body

--- blog-logo-ali/src/views/Home/index.jsx
+++ blog-logo-ali/src/views/Home/index.jsx
@@ -1,13 +1,28 @@
 import React from "react";
 import Nav from '../../components/Nav'
 import Footer from '../../components/Footer'
-
+import data from '../../data.json'
 import Sidebar from "../../components/SideBar";
+import Card from "../../components/Card"
+
+import './Home.css'
 
 function Home(){
     return(
         <main className="Home">      
             <Nav />
+
+            <section className="cards-container">
+                {data.map((prop)=>{
+                return <Card 
+                        imagem={prop.imagem}
+                        titulo={prop.titulo}
+                        descricao={prop.descricao}
+                        autor={prop.autor}
+                    />
+                })}
+            </section>
+
             <Footer />
             <Sidebar />
         </main>               
