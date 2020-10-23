81de044b95f853d90eec73d246a78a114c89dab8
thejanesol <janainapereiratelecom@gmail.com>
2020-10-23 11:02:08 -0300

    modificaçoes sidebar

--- blog-logo-ali/src/components/SideBar/index.jsx
+++ blog-logo-ali/src/components/SideBar/index.jsx
@@ -5,8 +5,17 @@ import './SideBar.css'
 const Sidebar = () => {
     return (
         <nav className="sidebar">
+            <h2>Postagens</h2>
             <ul className="posts-list">
-                <li>Viagem para Paris</li>
+                <ul class="month-list">
+                    <p>Outubro</p>
+                    <li>Viagem para Paris</li>
+                    <li>Viagem para Roma</li>
+                    <li>Viagem para NY</li>
+                    <li>Viagem para Orlando</li>
+                    <li>Rio de Janeiro</li>
+                    <li>Viagem para Praia de Iracema</li>
+                </ul>
             </ul>
         </nav>
     );
