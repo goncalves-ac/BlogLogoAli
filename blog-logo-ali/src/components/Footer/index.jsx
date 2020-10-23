92dc64465b48e6ec9419ee00115f5d60f97c58b8
goncalves-ac <goncalves.ac@outlook.com>
2020-10-23 10:53:05 -0300

    add footer

---
+++ blog-logo-ali/src/components/Footer/index.jsx
@@ -0,0 +1,12 @@
+import React from "react"
+import "./Footer.css"
+
+function Footer() {
+    return(
+        <footer className="Footer">
+            <p>Digital House / Santander - Todos os direitos reservados - Logo Ali 2020</p>
+        </footer>
+    )
+}
+
+export default Footer;
--- blog-logo-ali/src/views/Home/index.jsx
+++ blog-logo-ali/src/views/Home/index.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 import Nav from '../../components/Nav'
+import Footer from '../../components/Footer'
+
 
 function Home(){
     return(
         <main className="Home">      
             <Nav />
+            <Footer />
         </main>               
     )
 }
