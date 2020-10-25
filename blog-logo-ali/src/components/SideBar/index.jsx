0c7602df62ed00a6f2250aae8c481ba77e5f7cf4
Rayssa Ferreira <rayssaferr98@gmail.com>
2020-10-25 11:55:02 -0300

    enviando títulos das postagens via props

--- blog-logo-ali/src/components/SideBar/index.jsx
+++ blog-logo-ali/src/components/SideBar/index.jsx
@@ -1,24 +1,24 @@
 import React from "react";
-import {Link} from 'react-router-dom';
-import './SideBar.css'
+import './SideBar.css';
 
-const Sidebar = () => {
+const Sidebar = (props) => {
+    const data = props.data;
+
     return (
         <nav className="sidebar">
             <h2>Postagens</h2>
             <ul className="posts-list">
                 <ul className="month-list">
                     <p>Outubro</p>
-                    <li>Viagem para Paris</li>
-                    <li>Viagem para Roma</li>
-                    <li>Viagem para NY</li>
-                    <li>Viagem para Orlando</li>
-                    <li>Rio de Janeiro</li>
-                    <li>Viagem para Praia de Iracema</li>
+                    { 
+                        data.map((destino) => {
+                            return <li>{destino.titulo}</li>;
+                        })
+                    }
                 </ul>
             </ul>
         </nav>
     );
-}
+};
 
 export default Sidebar;
