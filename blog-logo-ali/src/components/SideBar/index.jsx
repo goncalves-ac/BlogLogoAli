f4650324c131f81c602357e4de750b45baad1151
Isabela Kunioka <isanakatani@hotmail.com>
2020-10-25 12:01:42 -0300

    footer e sidebar arrumados

--- blog-logo-ali/src/components/SideBar/index.jsx
+++ blog-logo-ali/src/components/SideBar/index.jsx
@@ -1,14 +1,15 @@
 import React from "react";
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import './SideBar.css'
 
 const Sidebar = () => {
     return (
         <nav className="sidebar">
-            <h2>Postagens</h2>
+            <h2>POSTAGENS</h2>
             <ul className="posts-list">
                 <ul className="month-list">
-                    <p>Outubro</p>
+                    <div className="mes"><p>Outubro</p></div>
+
                     <li>Viagem para Paris</li>
                     <li>Viagem para Roma</li>
                     <li>Viagem para NY</li>
