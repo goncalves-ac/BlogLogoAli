e25f9866e803adc5967f9aaee104cdf7d9b8f888
Matheus Siqueira <matheussiqueira.ec@gmail.com>
2020-10-25 14:54:11 -0300

    fix (routing): add base url to proper routing

--- src/routes.js
+++ src/routes.js
@@ -2,12 +2,12 @@ import React from "react";
 import { BrowserRouter, Route } from "react-router-dom";
 import Home from "./views/Home";
 
-function Routes(){
-    return(
-        <BrowserRouter>
-            <Route path="/" exact component={Home}/>
-        </BrowserRouter>
-    )
+function Routes() {
+  return (
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
+      <Route path="/" exact component={Home} />
+    </BrowserRouter>
+  );
 }
 
 export default Routes;
